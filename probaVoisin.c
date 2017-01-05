a26f52f1a6443dd563e71ecc7c776a055b721d11
Marylis Rubrice <marylis.rubrice@gmail.com>
2017-01-05 16:54:12 +0100

    Ajout de condition pour calculer les probabilités

--- probaVoisin.c
+++ probaVoisin.c
@@ -21,6 +21,63 @@ void proba voisin(int grille[NB_HT][NB_LG],int proba_bateau[NB_HT][NB_LG],int x,
 				if(grille[y+1][x]>-1)
 					proba_bateau[y+1][x]*=4;
 			}
+			 else if(y>0 && y<9)
+        {
+            if(x>0 && x<9)
+            {
+                if(grille[y][x-1]>-1)
+                    proba_bateau[y][x-1]*=4;
+                if(grille[y][x+1]>-1)
+                    proba_bateau[y][x+1]*=4;
+                if(grille[y-1][x]>-1)
+                    proba_bateau[y-1][x]*=4;
+                if(grille[y+1][x]>-1)
+                    proba_bateau[y+1][x]*=4;
+            }
+            if(x==0)
+            {
+                if(grille[y-1][x]>-1)
+                    proba_bateau[y-1][x]*=4;
+                if(grille[y+1][x]>-1)
+                    proba_bateau[y+1][x]*=4;
+                if(grille[y][x+1]>-1)
+                    proba_bateau[y][x+1]*=4;
+            }
+            if(x==9)
+            {
+                if(grille[y-1][x]>-1)
+                    proba_bateau[y-1][x]*=4;
+                if(grille[y+1][x]>-1)
+                    proba_bateau[y+1][x]*=4;
+                if(grille[y][x-1]>-1)
+                    proba_bateau[y][x-1]*=4;
+            }
+        }
+        else if(y==9)
+        {
+            if(x>0 && x<9)
+            {
+                if(grille[y][x-1]>-1)
+                    proba_bateau[y][x-1]*=4;
+                if(grille[y][x+1]>-1)
+                    proba_bateau[y][x+1]*=4;
+                if(grille[y-1][x]>-1)
+                    proba_bateau[y-1][x]*=4;
+            }
+            if(x==0)
+            {
+                if(grille[y-1][x]>-1)
+                    proba_bateau[y-1][x]*=4;
+                if(grille[y][x+1]>-1)
+                    proba_bateau[y][x+1]*=4;
+            }
+            if(x==9)
+            {
+                if(grille[y-1][x]>-1)
+                    proba_bateau[y-1][x]*=4;
+                if(grille[y][x-1]>-1)
+                    proba_bateau[y][x-1]*=4;
+            }
 		}
 	}
 }
